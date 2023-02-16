d343ce51e2ae6ddded9a45a2d17dff515342a7fc
thuotracy <tracyjacobs775@gmail.com>
2023-02-16 19:30:13 +0300

    made chnages

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 let navbar = document.querySelector('.head .navbar')
 
 document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.add('.active');
+    navbar.classList.add('active');
 }
 
 document.querySelector('#close-navbar').onclick = () =>{
-    navbar.classList.remove('.active');
+    navbar.classList.remove('active');
+}
+
+let registerBtn = document.querySelector('.account-form .register-btn');
+let loginBtn = document.querySelector('.account-form .login-btn');
+
+registerBtn.onclick = () =>{
+    registerBtn.classList.add('active');
+    loginBtn.classList.remove('active');
 }
