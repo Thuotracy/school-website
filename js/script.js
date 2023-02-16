ef9db6f00939dea71f9657bd0dea05f4557a0e13
thuotracy <tracyjacobs775@gmail.com>
2023-02-16 18:14:53 +0300

    created the close hamburger menu btn

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@ let navbar = document.querySelector('.head .navbar')
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.add('.active');
 }
+
+document.querySelector('#close-navbar').onclick = () =>{
+    navbar.classList.remove('.active');
+}
