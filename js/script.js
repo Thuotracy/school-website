06d8e6b04e0c1def93ce2db8aee7a4ced3acb6e1
thuotracy <tracyjacobs775@gmail.com>
2023-02-27 18:13:04 +0300

    finished styling the faq section

--- js/script.js
+++ js/script.js
@@ -115,7 +115,16 @@ var swiper = new Swiper(".reviews-slider",{
 });
 
 
-// load more courses btn
+// accordion content
+let accordion = document.querySelectorAll('.faq .accordion-container .accordion');
+
+accordion.forEach(acco => {
+  acco.onclick = () => {
+    accordion.forEach(dion => dion.classList.remove('active'));
+    acco.classList.toggle('active');
+  }
+});
+
 document.querySelector('.load-more .btn').onclick = () => {
   document.querySelectorAll('.courses .box-container .hide').forEach(show =>{
     show.style.display = 'block';
