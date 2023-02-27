749864e120d0631c695d2e3aaf3db41732860281
thuotracy <tracyjacobs775@gmail.com>
2023-02-27 14:48:39 +0300

    finished styling the reviews section

--- js/script.js
+++ js/script.js
@@ -88,3 +88,20 @@ var swiper = new Swiper(".teachers-slider",{
   },
 });
 
+var swiper = new Swiper(".reviews-slider",{
+  loop:true,
+  grabCursor:true,
+  spaceBetween: 20,
+  breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+  },
+});
+
