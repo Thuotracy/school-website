9916dd6b3ddfa66093a397840b1682b5d0e5c1cb
thuotracy <tracyjacobs775@gmail.com>
2023-02-27 13:17:40 +0300

    finished the teachers section

--- js/script.js
+++ js/script.js
@@ -71,3 +71,20 @@ var swiper = new Swiper(".home-course-slider",{
     },
 });
 
+var swiper = new Swiper(".teachers-slider",{
+  loop:true,
+  grabCursor:true,
+  spaceBetween: 20,
+  breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+  },
+});
+
