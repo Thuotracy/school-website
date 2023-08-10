f72e453ef0dbcfdc05ef5cf06a38aee523aa39f9
Peter Kimanzi <71552773+peter-kimanzi@users.noreply.github.com>
2023-08-10 17:28:33 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.add('active');
 }
 
+
 document.querySelector('#close-navbar').onclick = () =>{
     navbar.classList.remove('active');
 }
